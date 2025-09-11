4bdeb4699c2202ff60c202807ce59ce52b02b117
shivani parmar <shivaniparmar316@gmail.com>
2025-09-11 20:01:45 +0530

    contact us page expect the form section

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,17 @@
+import ContactHero from "../components/sections/contactPage/ContactHero";
+import Faq from "../components/sections/contactPage/Faq";
+import GetStarted from "../components/sections/contactPage/GetStarted";
+import LocationSection from "../components/sections/contactPage/LocationSection";
+
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+    <ContactHero/>
+    <LocationSection/>
+    <Faq/>
+    <GetStarted/>
+    </>
+  )
 }
 
 export default Contact;
